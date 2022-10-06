40b3441d662ace734622e6428932de9e89203ff5
matheuskrieck <110129434+matheuskrieck@users.noreply.github.com>
2022-10-05 21:04:59 -0300

    update form validation

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const form = document.querySelector("form");
 const email = document.getElementById("email");
-const error = document.getElementById("form-error");
 const errorType = document.getElementById("error-type");
 const regexp = /^\S+@\S+\.\S+$/;
 
 email.addEventListener("invalid", () => {
     if (email.value == "") {
-        errorType.innerHTML = "add";
+        errorType.innerHTML = "Oops! Please add your email !";
     } else if (regexp.test(email) == false ) {
-        errorType.innerHTML = "check";   
+        errorType.innerHTML = "Oops! Please check your email !";   
     }
-    error.classList.remove("hide");
 })
 
 email.addEventListener("input", () => {
-    error.classList.add("hide");
+    errorType.innerHTML = "";
 });
 
 form.addEventListener("submit", (e) => {
-    console.log(e)
     e.preventDefault()
-     alert("Your form has been submitted, Thank you for testing!")
+    alert("Your form has been submitted, Thank you for testing!")
     email.value = ""
-    error.classList.add("hide");
 });
